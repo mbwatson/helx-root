eefd3a4c6fac32c291a89ab408dd1e634a007ae2
Matt Watson <mattwat@gmail.com>
2021-07-31 16:33:22 -0400

    general enhancements

--- src/app.js
+++ src/app.js
@@ -2,37 +2,40 @@ import React, { Suspense } from 'react'
 import { Layout, Menu } from 'antd'
 import "antd/dist/antd.css";
 import { Link, Router } from '@reach/router'
+import HeLxLogo from './images/helx-logo.png'
 import './app.scss'
-
 const { Content } = Layout
 const { SubMenu } = Menu
 
+const Home           = () => <div><h1>HOME</h1></div>
+const Workspaces     = () => <div><h1>WORKSPACES</h1></div>
 const SemanticSearch = React.lazy(() => import('search/App'))
-const Home      = () => <div><h1>HOME</h1></div>
-const SearchApp = () => <div><h1>SEARCH</h1></div>
-const AppOne    = () => <div><h1>APP ONE</h1></div>
-const AppTwo    = () => <div><h1>APP TWO</h1></div>
-const AppThree  = () => <div><h1>APP THREE</h1></div>
+
+const Brand = () => {
+  return (
+    <div className="brand">
+      <img src={ HeLxLogo } width="80" className="logo" />
+      <span className="name">The HEAL Initiative</span>
+    </div>
+  )
+}
 
 export const App = () => {
   return (
     <div className="app">
       <div className="main-header">
-        <Menu mode="horizontal">
-          <Menu.Item><Link to="/">HOME</Link></Menu.Item>
-          <Menu.Item><Link to="/1">ONE</Link></Menu.Item>
-          <Menu.Item><Link to="/2">TWO</Link></Menu.Item>
-          <Menu.Item><Link to="/3">THREE</Link></Menu.Item>
-          <Menu.Item><Link to="/search">SEARCH</Link></Menu.Item>
+        <Brand />
+        <Menu mode="horizontal" className="main-menu">
+          <Menu.Item><Link to="/" className="menu-item">HOME</Link></Menu.Item>
+          <Menu.Item><Link to="/workspaces" className="menu-item">WORKSPACES</Link></Menu.Item>
+          <Menu.Item><Link to="/search" className="menu-item">SEARCH</Link></Menu.Item>
         </Menu>
       </div>
       <div className="main-content">
         <Suspense fallback={ <div className="loading">Loading... </div> }>
           <Router>
             <Home exact path="/" />
-            <AppOne exact path="/1" />
-            <AppTwo exact path="/2" />
-            <AppThree exact path="/3" />
+            <Workspaces exact path="/workspaces" />
             <SemanticSearch exact path="/search" />
           </Router>
         </Suspense>
